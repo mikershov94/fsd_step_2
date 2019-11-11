28445b97c1d1a7dab96617713ffce8a4461265c1
mikershov94 <ershovme@gmail.com>
2019-11-11 19:45:03 +0800

    Configured webpack. Dev-config

--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -1,3 +1,21 @@
+const path = require('path');
+
 module.exports = {
     mode: 'development',
+
+    devtool: 'cheap-inline-module-source-map',
+
+    devServer: {
+        contentBase: path.join(__dirname, '../'),
+        overlay: true,
+        stats: 'errors-only',
+        port: 3000,
+        historyApiFallback: true,
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    }
 }
