24280e528d29327813e1c81a724befb524b14fdb
mikershov94 <ershovme@gmail.com>
2019-12-02 13:40:18 +0800

    Added caching in webpack build config

--- config/webpack.build.conf.js
+++ config/webpack.build.conf.js
@@ -2,5 +2,20 @@ const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf.js');
 
 module.exports = merge(baseWebpackConfig, {
-    mode: 'production'
+    mode: 'production',
+
+    optimization: {
+        moduleIds: 'hashed',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+
 })
