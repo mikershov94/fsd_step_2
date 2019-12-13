0e7e6f85980b1f0996c7d0587338ea266bc4430b
mikershov94 <ershovme@gmail.com>
2019-12-13 19:30:41 +0800

    Refactored filter dropdown. Added close handlers

--- src/components/form-elements/filter-dropdown/filter-dropdown.js
+++ src/components/form-elements/filter-dropdown/filter-dropdown.js
@@ -2,6 +2,7 @@
 
     $.fn.filterDropdown = function() {
 
+        //обработчик клика по Dropdown
         $(this).on('click', function() {
 
             const stateDropdown = $(this).attr('data-expand');
@@ -9,11 +10,21 @@
                 $(this).children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
                 $(this).attr('data-expand', '');
             } else {
+                $('.filter-dropdown').children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
+                $('.filter-dropdown').attr('data-expand', '');
                 $(this).children('.filter-dropdown__list').addClass('filter-dropdown__list_expanded');
                 $(this).attr('data-expand', 'expanded');
             }
 
         })
+
+        //обработчик клика за пределами Dropdown
+        $(document).click(function(e) {
+            if ($(e.target).closest('.filter-dropdown').length) return;
+            $('.filter-dropdown').children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
+            e.stopPropagation()
+        })
+
     }
 
 })(jQuery)
