b0e1739c79ee77700951cc6d05f66f0e4331ff5c
mikershov94 <ershovme@gmail.com>
2019-12-14 22:50:22 +0800

    Created filter-dropdown counters

--- src/components/form-elements/filter-dropdown/filter-dropdown.js
+++ src/components/form-elements/filter-dropdown/filter-dropdown.js
@@ -1,20 +1,70 @@
 (function($) {
 
     $.fn.filterDropdown = function() {
+        
+        const bedroomsPanel = $(this).find('#bedrooms .filter-dropdown__count');
+        const bedsPanel = $(this).find('#beds .filter-dropdown__count');
+        const bathroomsPanel = $(this).find('#bathrooms .filter-dropdown__count');
+        
+        let bedrooms = bedroomsPanel.attr('data-value');
+        let beds = bedsPanel.attr('data-value');
+        let bathrooms = bathroomsPanel.attr('data-value');
 
+        
+        //обработчик клика Bedrooms Increment
+        $(this).on('click', '#bedrooms .filter-dropdown__btn_inc', function() {
+            bedrooms++;
+            bedroomsPanel.attr('data-value', bedrooms);
+            bedroomsPanel.empty()
+            bedroomsPanel.html(bedrooms);
+        })
+
+        //обработчик клика Bedrooms Decrement
+        $(this).on('click', '#bedrooms .filter-dropdown__btn_dec', function() {
+            bedrooms--;
+            bedroomsPanel.attr('data-value', bedrooms);
+            bedroomsPanel.empty()
+            bedroomsPanel.html(bedrooms);
+        })
+
+        //обработчик клика Beds Increment
+        $(this).on('click', '#beds .filter-dropdown__btn_inc', function() {
+            beds++;
+            bedsPanel.attr('data-value', beds);
+            bedsPanel.empty()
+            bedsPanel.html(beds);
+        })
+
+        //обработчик клика Beds Decrement
+        $(this).on('click', '#beds .filter-dropdown__btn_dec', function() {
+            beds--;
+            bedsPanel.attr('data-value', beds);
+            bedsPanel.empty()
+            bedsPanel.html(beds);
+        })
+
+        //обработчик клика Bedrooms Increment
+        $(this).on('click', '#bathrooms .filter-dropdown__btn_inc', function() {
+            bathrooms++;
+            bathroomsPanel.attr('data-value', bathrooms);
+            bathroomsPanel.empty()
+            bathroomsPanel.html(bathrooms);
+        })
+
+        //обработчик клика Bedrooms Decrement
+        $(this).on('click', '#bathrooms .filter-dropdown__btn_dec', function() {
+            bathrooms--;
+            bathroomsPanel.attr('data-value', bathrooms);
+            bathroomsPanel.empty()
+            bathroomsPanel.html(bathrooms);
+        })
+    
+        
         //обработчик клика по Dropdown
-        $(this).on('click', function() {
+        $(this).on('click', function(e) {
 
-            const stateDropdown = $(this).attr('data-expand');
-            if (stateDropdown === 'expanded') {
-                $(this).children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
-                $(this).attr('data-expand', '');
-            } else {
-                $('.filter-dropdown').children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
-                $('.filter-dropdown').attr('data-expand', '');
-                $(this).children('.filter-dropdown__list').addClass('filter-dropdown__list_expanded');
-                $(this).attr('data-expand', 'expanded');
-            }
+            $('.filter-dropdown').children('.filter-dropdown__list').removeClass('filter-dropdown__list_expanded');
+            $(this).children('.filter-dropdown__list').addClass('filter-dropdown__list_expanded');
 
         })
 
