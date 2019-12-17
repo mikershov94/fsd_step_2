181ba2e00c8826508de4f496f6811ac6557a9e86
mikershov94 <ershovme@gmail.com>
2019-12-17 18:46:40 +0800

    Refactored filterDropdown js. Deleted unused strings

--- src/components/form-elements/filter-dropdown/filter-dropdown.js
+++ src/components/form-elements/filter-dropdown/filter-dropdown.js
@@ -4,6 +4,9 @@
 
         const updateDropdownContent = (i, value, str) => {
             filterState[i] = value + ' ' + str;
+            if (value === 0) {
+                filterState[i] = '';
+            }
             if (filterState[1] !== '' || filterState[2] !== '') {
                 if (filterState[0] !== '') {
                     filterState[0] = filterState[0].replace(/,\s/, '') + ', ';    
@@ -22,17 +25,24 @@
             filterString = filterState[0] + filterState[1] + filterState[2];
             
         };
+
+        const cuttingString = (str) => {
+
+            if (str.length > 20) {
+                return str.substr(0, 19) + '...';
+            } else {
+                return str;
+            }
+
+        }
         
         const bedroomsPanel = $(this).find('#bedrooms .filter-dropdown__count');
         const bedsPanel = $(this).find('#beds .filter-dropdown__count');
         const bathroomsPanel = $(this).find('#bathrooms .filter-dropdown__count');
         
         let bedrooms = bedroomsPanel.attr('data-value');
-        
         let beds = bedsPanel.attr('data-value');
-        let bedsStr = bedsPanel.attr('data-string');
         let bathrooms = bathroomsPanel.attr('data-value');
-        let bathroomsStr = bathroomsPanel.attr('data-string');
 
         //инициализируем строку дропдауна
         let filterString = '';
@@ -46,8 +56,6 @@
         //обработчик клика Bedrooms Increment
         $(this).on('click', '#bedrooms .filter-dropdown__btn_inc', function() {
             bedrooms++;
-            let bedroomsStr = bedroomsPanel.attr('data-string');
-            let content
             bedroomsPanel.attr('data-value', bedrooms);
             //склоняем "Спальни"
             if (bedrooms === 1) {
@@ -59,16 +67,8 @@
             }
 
             updateDropdownContent(0, bedrooms, bedroomsStr);
-            console.log(filterString)
+            cutString = cuttingString(filterString);
 
-
-            if (filterString.length > 20) {
-                cutString = filterString.substr(0, 19) + '...';
-            } else {
-                cutString = filterString;
-            }
-
-            bedroomsPanel.attr('data-string', bedroomsStr);
             bedroomsPanel.empty()
             bedroomsPanel.html(bedrooms);
             $(this).closest('.filter-dropdown').find('.filter-dropdown__content').empty();
@@ -87,16 +87,19 @@
             } else {
                 bedroomsStr = 'спален'
             }
-            //bedroomsPanel.attr('data-string', bedroomsStr);
+
+            updateDropdownContent(0, bedrooms, bedroomsStr);
+            cutString = cuttingString(filterString);
+
             bedroomsPanel.empty()
             bedroomsPanel.html(bedrooms);
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').empty();
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').html(cutString);
         })
 
         //обработчик клика Beds Increment
         $(this).on('click', '#beds .filter-dropdown__btn_inc', function() {
             beds++;
-            let bedsStr = bedsPanel.attr('data-string');
-            let content
             bedsPanel.attr('data-value', beds);
             //склоняем "Кровати"
             if (beds === 1) {
@@ -108,16 +111,8 @@
             }
 
             updateDropdownContent(1, beds, bedsStr);
-            console.log(filterString)
+            cutString = cuttingString(filterString);
 
-            if (filterString.length > 20) {
-                cutString = filterString.substr(0, 19) + '...';
-            } else {
-                cutString = filterString;
-            }
-
-
-            bedsPanel.attr('data-string', bedsStr);
             bedsPanel.empty()
             bedsPanel.html(beds);
             $(this).closest('.filter-dropdown').find('.filter-dropdown__content').empty();
@@ -136,16 +131,20 @@
             } else {
                 bedsStr = 'кроватей'
             }
-            bedsPanel.attr('data-string', bedsStr);
+
+            updateDropdownContent(1, beds, bedsStr);
+            cutString = cuttingString(filterString);
+
             bedsPanel.empty()
             bedsPanel.html(beds);
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').empty();
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').html(cutString);
         })
 
         //обработчик клика Bathrooms Increment
         $(this).on('click', '#bathrooms .filter-dropdown__btn_inc', function() {
             bathrooms++;
             let bathroomsStr = bathroomsPanel.attr('data-string');
-            let content
             bathroomsPanel.attr('data-value', bathrooms);
             //склоняем "Ванные комнаты"
             if (bathrooms === 1) {
@@ -157,13 +156,7 @@
             }
 
             updateDropdownContent(2, bathrooms, bathroomsStr);
-            console.log(filterString)
-
-            if (filterString.length > 20) {
-                cutString = filterString.substr(0, 19) + '...';
-            } else {
-                cutString = filterString;
-            }
+            cutString = cuttingString(filterString);
 
             bathroomsPanel.attr('data-string', bathroomsStr);
             bathroomsPanel.empty()
@@ -184,10 +177,14 @@
             } else {
                 bathroomsStr = 'ванных комнат'
             }
-            bathroomsPanel.attr('data-string', bathroomsStr);
-            bathroomsPanel.empty()
+
+            updateDropdownContent(2, bathrooms, bathroomsStr);
+            cutString = cuttingString(filterString);
+
             bathroomsPanel.empty()
             bathroomsPanel.html(bathrooms);
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').empty();
+            $(this).closest('.filter-dropdown').find('.filter-dropdown__content').html(cutString);
         })
     
         
