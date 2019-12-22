0685229445b6824e019092087446ec2395381813
mikershov94 <ershovme@gmail.com>
2019-12-22 15:38:14 +0800

    Fixed errors

--- src/components/form-elements/date-dropdown/date-dropdown.js
+++ src/components/form-elements/date-dropdown/date-dropdown.js
@@ -1,1 +1,14 @@
 import 'air-datepicker/dist/js/datepicker.min';
+
+$(document).ready(function() {
+    $('.date-dropdown').on('click', function(e) {
+        console.log($(this))
+        $(this).addClass('datepicker-here');
+    })
+
+    $(document).click(function(e) {
+        if ($(e.target).closest('.date-dropdown').length) return;
+        $('.date-dropdown').removeClass('datepicker-here');
+        e.stopPropagation()
+    })
+})
