e6ca74fdd6c757a20dad49bed865b57ae03e2a39
mikershov94 <ershovme@gmail.com>
2019-12-22 11:51:33 +0800

    Begin create date-dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './main.sass';
 import 'jquery-mask-plugin';
 import './components/form-elements/textfield-masked/textfield-masked.js';
-import './components/form-elements/filter-dropdown/filter-dropdown.js'
+import './components/form-elements/filter-dropdown/filter-dropdown.js';
+import './components/form-elements/date-dropdown/date-dropdown.js';
