cc103bbc7c7281812dd59743af0731db079c68fd
mikershov94 <ershovme@gmail.com>
2019-12-29 23:24:43 +0800

    Added like buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import 'jquery-mask-plugin';
 import './components/form-elements/textfield-masked/textfield-masked.js';
 import './components/form-elements/filter-dropdown/filter-dropdown.js';
 import './components/form-elements/date-dropdown/date-dropdown.js';
+
+$(document).ready(function() {
+    $('.like-button').each(function() {
+        if ($(this).attr('data-state') === 'liked') {
+            $(this).addClass('like-button_liked');
+            $(this).children('i').addClass('like-button__icon_liked');
+            $(this).children('i').text('favorite');
+            $(this).children('.like-button__count').addClass('like-button__count_liked')
+        }
+    })
+
+    $('.like-button').on('click', function() {
+        $(this).attr('data-state', 'liked');
+        $(this).addClass('like-button_liked');
+        $(this).children('i').addClass('like-button__icon_liked');
+        $(this).children('i').text('favorite');
+        $(this).children('.like-button__count').addClass('like-button__count_liked')
+    })
+})
