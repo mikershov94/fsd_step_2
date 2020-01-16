38c165bddb0afe348403c74ab7a9e6de8656b20e
mikershov94 <ershovme@gmail.com>
2020-01-16 18:21:33 +0800

    Add nouislider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './components/form-elements/textfield-masked/textfield-masked.js';
 import './components/form-elements/filter-dropdown/filter-dropdown.js';
 import './components/form-elements/date-dropdown/date-dropdown.js';
 
+import noUiSlider from 'nouislider';
+
 $(document).ready(function() {
     $('.like-button').each(function() {
         if ($(this).attr('data-state') === 'liked') {
@@ -21,4 +23,16 @@ $(document).ready(function() {
         $(this).children('i').text('favorite');
         $(this).children('.like-button__count').addClass('like-button__count_liked')
     })
-})
+});
+
+var slider = document.getElementById('slider');
+console.log(slider)
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
