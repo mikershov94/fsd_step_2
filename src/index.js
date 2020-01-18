9bdab9c6bc00dbaf5b46ee6dd1917152f294b3f0
mikershov94 <ershovme@gmail.com>
2020-01-18 14:02:17 +0800

    Added formatted values in HTML

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './components/form-elements/filter-dropdown/filter-dropdown.js';
 import './components/form-elements/date-dropdown/date-dropdown.js';
 
 import noUiSlider from 'nouislider';
+import wNumb from 'wnumb';
 
 $(document).ready(function() {
     $('.like-button').each(function() {
@@ -25,14 +26,25 @@ $(document).ready(function() {
     })
 });
 
-var slider = document.getElementById('slider');
-console.log(slider)
+const slider = document.getElementById('slider');
+const rangeValues = [
+    document.getElementById('values__low'),
+    document.getElementById('values__high')
+];
 
 noUiSlider.create(slider, {
-    start: [27, 60],
+    start: [5000, 10000],
     connect: true,
     range: {
         'min': 0,
-        'max': 100
-    }
+        'max': 15000
+    },
+    format: wNumb({
+        decimals: 0,
+        suffix: '₽'
+    })
+});
+
+slider.noUiSlider.on('update', function(values, handle) {
+    rangeValues[handle].innerHTML = values[handle];
 });
