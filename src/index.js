6d2d915f8cf5e4f119c19c5545885ec84a83f32e
mikershov94 <ershovme@gmail.com>
2020-01-18 14:14:12 +0800

    Refactored range-slider to component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './main.sass';
 import 'jquery-mask-plugin';
+
 import './components/form-elements/textfield-masked/textfield-masked.js';
 import './components/form-elements/filter-dropdown/filter-dropdown.js';
 import './components/form-elements/date-dropdown/date-dropdown.js';
 
-import noUiSlider from 'nouislider';
-import wNumb from 'wnumb';
+import './components/form-elements/range-slider/range-slider.js'
 
 $(document).ready(function() {
     $('.like-button').each(function() {
@@ -24,27 +24,4 @@ $(document).ready(function() {
         $(this).children('i').text('favorite');
         $(this).children('.like-button__count').addClass('like-button__count_liked')
     })
-});
-
-const slider = document.getElementById('slider');
-const rangeValues = [
-    document.getElementById('values__low'),
-    document.getElementById('values__high')
-];
-
-noUiSlider.create(slider, {
-    start: [5000, 10000],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 15000
-    },
-    format: wNumb({
-        decimals: 0,
-        suffix: '₽'
-    })
-});
-
-slider.noUiSlider.on('update', function(values, handle) {
-    rangeValues[handle].innerHTML = values[handle];
 });
