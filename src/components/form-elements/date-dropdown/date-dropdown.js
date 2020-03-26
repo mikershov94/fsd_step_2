62a9951fb60b4797a39ccd1f3ef0101cb81726a0
mikershov94 <ershovme@gmail.com>
2020-03-26 15:00:13 +0800

    Added datepicker to component

--- src/components/form-elements/date-dropdown/date-dropdown.js
+++ src/components/form-elements/date-dropdown/date-dropdown.js
@@ -1,14 +1,5 @@
 import 'air-datepicker/dist/js/datepicker.min';
 
-$(document).ready(function() {
-    $('.date-dropdown').on('click', function(e) {
-        console.log($(this))
-        $(this).addClass('datepicker-here');
-    })
-
-    $(document).click(function(e) {
-        if ($(e.target).closest('.date-dropdown').length) return;
-        $('.date-dropdown').removeClass('datepicker-here');
-        e.stopPropagation()
-    })
+$(() => {
+    $(".date-dropdown__input").mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ"});
 })
