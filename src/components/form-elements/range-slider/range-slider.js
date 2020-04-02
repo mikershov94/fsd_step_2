9ab9d9ffb335880c877928831a5dca64cd3ba639
mikershov94 <ershovme@gmail.com>
2020-04-02 14:14:40 +0800

    Fixed errors in range-slider

--- src/components/form-elements/range-slider/range-slider.js
+++ src/components/form-elements/range-slider/range-slider.js
@@ -1,26 +1,38 @@
 import noUiSlider from 'nouislider';
 import wNumb from 'wnumb';
 
-const slider = document.getElementById('slider');
-const rangeValues = [
-    document.getElementById('values__low'),
-    document.getElementById('values__high')
-];
+(function($) {
 
-noUiSlider.create(slider, {
-    start: [5000, 10000],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 15000
-    },
-    format: wNumb({
-        decimals: 0,
-        thousand: ' ',
-        suffix: '₽'
-    })
-});
+    $.fn.rangeSlider = function() {
+        return this.each(function() {
+            const slider = this.querySelector('#slider');
+            const rangeValues = [
+                this.querySelector('#values__low'),
+                this.querySelector('#values__high')
+            ];
 
-slider.noUiSlider.on('update', function(values, handle) {
-    rangeValues[handle].innerHTML = values[handle];
+            noUiSlider.create(slider, {
+                start: [5000, 10000],
+                connect: true,
+                range: {
+                    'min': 0,
+                    'max': 15000
+                },
+                format: wNumb({
+                    decimals: 0,
+                    thousand: ' ',
+                    suffix: '₽'
+                })
+            });
+
+            slider.noUiSlider.on('update', function(values, handle) {
+                rangeValues[handle].innerHTML = values[handle];
+            });
+        });
+    }
+
+})(jQuery);
+
+$(document).ready(() => {
+    $('.range-slider').rangeSlider();
 });
