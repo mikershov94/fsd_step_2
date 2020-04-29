8c8db0044568aa182afe7fc849bf614b79c830ae
mikershov94 <ershovme@gmail.com>
2020-04-29 14:28:31 +0800

    Update webpack-config

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -79,8 +79,7 @@ module.exports = {
 					{
 						loader: 'file-loader',
 						options: {
-							name: '[name].[ext]',
-							outputPath: 'fonts/'
+							name: 'fonts/[name].[ext]',
 						}
 					}
 				]
